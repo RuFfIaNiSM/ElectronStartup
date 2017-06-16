48d1d380ec6bdeb44d6ff4322c83faf9e3e884fe
David Brierton <davidbrierton@msn.com>
2017-06-16 09:03:18 -0400

    Update index.js

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ let mainWindow;
 app.on('ready', () => {
   mainWindow = new BrowserWindow({});
   mainWindow.loadURL(`file://${__dirname}/main.html`);
+  mainWindow.on('closed', () => app.quit());
 
   const mainMenu = Menu.buildFromTemplate(menuTemplate);
   Menu.setApplicationMenu(mainMenu);
@@ -30,4 +31,23 @@ const menuTemplate = [
 
 if (process.platform === 'darwin') {
   menuTemplate.unshift({});
+}
+
+// 'production'
+// 'development'
+// 'staging'
+// 'test'
+if (process.env.NODE_ENV !== 'production') {
+  menuTemplate.push({
+    label: 'Developer',
+    submenu: [
+      {
+        label: 'Toggle Developer Tools',
+        accelerator: process.platform === 'darwin' ? 'Command+Alt+I' : 'F12',
+        click(item, focusedWindow) {
+          focusedWindow.toggleDevTools();
+        }
+      }
+    ]
+  });
 }
